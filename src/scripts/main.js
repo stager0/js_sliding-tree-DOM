bb7d5e9b2777a5330924a301d4887923cfc72a86
Andrii <andriishtaher@gmail.com>
2025-10-23 01:42:42 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const allTreeChild = [];
+const allSiblings = [];
+
+for (const child of tree.children) {
+  allTreeChild.push(child);
+
+  for (const sibling of child.querySelector('ul').children) {
+    allSiblings.push(sibling);
+  }
+}
+
+function hideLi(liToHide) {
+  const newSpan = document.createElement('span');
+
+  let textContent = '123';
+  const textNode = Array.from(liToHide.childNodes).find(
+    (node) =>
+      node.nodeType === Node.TEXT_NODE && node.textContent.trim().length > 0,
+  );
+
+  if (!textNode) {
+    return;
+  }
+
+  textContent = liToHide.firstChild.textContent;
+  liToHide.firstChild.remove();
+
+  newSpan.textContent = textContent;
+
+  liToHide.prepend(newSpan);
+
+  newSpan.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const li = e.target.closest('li');
+    const childUl = li.querySelector('ul');
+
+    childUl.classList.toggle('isHide');
+
+    if (childUl.classList.contains('isHide')) {
+      childUl.style.display = 'none';
+    } else {
+      childUl.style.display = '';
+    }
+  });
+}
+
+allSiblings.forEach((sib) => {
+  hideLi(sib);
+});
+
+allTreeChild.forEach((child) => hideLi(child));
