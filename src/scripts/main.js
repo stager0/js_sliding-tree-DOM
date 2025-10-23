5a710d6dfb9bbd496e84c5f56f7ccd5fa2c03af2
Andrii <andriishtaher@gmail.com>
2025-10-23 02:23:57 +0200

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,18 +4,21 @@ const tree = document.querySelector('.tree');
 const allTreeChild = [];
 const allSiblings = [];
 
-for (const child of tree.children) {
-  allTreeChild.push(child);
+if (tree.children) {
+  for (const child of tree.children) {
+    allTreeChild.push(child);
 
-  for (const sibling of child.querySelector('ul').children) {
-    allSiblings.push(sibling);
+    if (child.querySelector('ul').children) {
+      for (const sibling of child.querySelector('ul').children) {
+        allSiblings.push(sibling);
+      }
+    }
   }
 }
 
 function hideLi(liToHide) {
   const newSpan = document.createElement('span');
 
-  let textContent = '123';
   const textNode = Array.from(liToHide.childNodes).find(
     (node) =>
       node.nodeType === Node.TEXT_NODE && node.textContent.trim().length > 0,
@@ -25,8 +28,9 @@ function hideLi(liToHide) {
     return;
   }
 
-  textContent = liToHide.firstChild.textContent;
-  liToHide.firstChild.remove();
+  const textContent = textNode.textContent;
+
+  textNode.remove();
 
   newSpan.textContent = textContent;
 
@@ -38,12 +42,14 @@ function hideLi(liToHide) {
     const li = e.target.closest('li');
     const childUl = li.querySelector('ul');
 
-    childUl.classList.toggle('isHide');
+    if (childUl) {
+      childUl.classList.toggle('isHide');
 
-    if (childUl.classList.contains('isHide')) {
-      childUl.style.display = 'none';
-    } else {
-      childUl.style.display = '';
+      if (childUl.classList.contains('isHide')) {
+        childUl.style.display = 'none';
+      } else {
+        childUl.style.display = '';
+      }
     }
   });
 }
